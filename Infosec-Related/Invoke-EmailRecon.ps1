2b0094bae2e3ad3a9cb6024e19b86b108ad6334d
Daniel Streefkerk <dstreefkerk@users.noreply.github.com>
2019-02-09 13:13:10 +1100

    Update Invoke-EmailRecon.ps1
    1. Refactored to cut down on the number of DNS queries the script has to make. Now 55% faster
    2. Added checks for MTA-STS record
    3. Added visibility around SPF and DKIM modes

--- Infosec-Related/Invoke-EmailRecon.ps1
+++ Infosec-Related/Invoke-EmailRecon.ps1
@@ -3,7 +3,7 @@
 	A quick and dirty script to be used to automate the collection of 
     publicly-available email-related records
 
-    For example; MX, SPF, DMARC records.
+    For example; MX, SPF, DMARC, HTA-STS records.
 
     It'll also try to make a determination about who's handling mail flow, and whether the domain
     is hosted on Exchange Online (sometimes ExOnline tenants pass their mail through filtering
@@ -42,6 +42,11 @@
 
 	Get a list of domains from a text file (single domain per line), retrieve the details, and export the results to a CSV file
 
+.EXAMPLE
+	Import-Csv C:\temp\companies.csv | Select-Object -ExpandProperty Email_Domain | C:\Scripts\Invoke-EmailRecon.ps1 | Out-GridView
+
+	Get a list of domains from a CSV file that contains a column named 'Email_Domain, run our process across each one of them, and output the results to a GridView GUI control
+
 .INPUTS
 	System.String
 
@@ -59,11 +64,15 @@
 
 	VERSION HISTORY:
 		1.0 05/02/2019
-			Initial Version, based on an old collection of stuff I've had lying around for ages
+			- Initial Version, based on an old collection of stuff I've had lying around for ages
+
+        1.0 09/02/2019
+            - Refactored to cut down on the number of DNS queries the script has to make. 55% increase in speed.
+            - Added checks for MTA-STS record
+            - Added visibility around SPF and DKIM modes
 			
 	TODO:
-		- The code needs to be optimised as we're making many DNS queries for each domain. That'll come later in my rebuilt
-          version of this script. I've been working on a modular version that uses plugins for some time now.
+		- Add code comments and verbose logging
 #>
 
 
@@ -84,81 +93,98 @@ begin {
         throw "PowerShell 3.0 is required due to this script's use of ordered psobjects"
     }
 
-    function Check-MxRecord ([string]$DomainName) {
-        if ([string]::IsNullOrEmpty($DomainName)) {
-            throw "No domain name specified for MX record check"
-        }
-
-        Resolve-DnsName -Name $DomainName -Type MX -ErrorAction SilentlyContinue -WarningAction SilentlyContinue
+    function Check-SpfRecordExists ([psobject]$DNSData) {
+        $DNSData.TXT | Where-Object {$_.Strings -like '*v=spf1*'} -ErrorAction SilentlyContinue
     }
 
-    function Check-SpfRecord ([string]$DomainName) {
-        if ([string]::IsNullOrEmpty($DomainName)) {
-            throw "No domain name specified for SPF record check"
-        }
-
-        $resolved = Resolve-DnsName -Name $DomainName -Type TXT #-ErrorAction SilentlyContinue -WarningAction SilentlyContinue
-
-        $resolved | Where-Object {$_.Strings -like '*v=spf1*'}
+    function Get-SpfRecordText ([psobject]$DNSData) {
+        $DNSData.TXT | Where-Object {$_.Strings -like '*v=spf1*'} -ErrorAction SilentlyContinue | Select-Object -First 1 -ExpandProperty Strings -ErrorAction SilentlyContinue
     }
 
-    function Get-SpfRecordText ([string]$DomainName) {
-        if ([string]::IsNullOrEmpty($DomainName)) {
-            throw "No domain name specified for SPF record check"
+    function Determine-SpfRecordMode ([psobject]$DNSData) {
+        $record = $DNSData.TXT | Where-Object {$_.Strings -like '*v=spf1*'} -ErrorAction SilentlyContinue
+
+        if ($record) {
+            $recordData = $record | Select-Object -First 1 -ExpandProperty Strings
+            switch -Wildcard ($recordData) {
+                '*-all' { $determination = "FAIL" }
+                '*+all' { $determination = "PASS" }
+                '*~all' { $determination = "SOFTFAIL" }
+                '*`?all' { $determination = "NEUTRAL" }
+
+                Default { $determination = "Other/Undetermined" }
+            }
+
+            return $determination
+        } else {
+            return "N/A"
         }
-
-        $resolved = Resolve-DnsName -Name $DomainName -Type TXT #-ErrorAction SilentlyContinue -WarningAction SilentlyContinue
-
-        ($resolved | Where-Object {$_.Strings -like '*v=spf1*'}) | Select-Object -ExpandProperty Strings
     }
 
-    function Check-DmarcRecord ([string]$DomainName) {
-        if ([string]::IsNullOrEmpty($DomainName)) {
-            throw "No domain name specified for DMARC record check"
-        }
-
-        $resolved = Resolve-DnsName -Name "_dmarc.$($DomainName)" -Type TXT -ErrorAction SilentlyContinue -WarningAction SilentlyContinue
-
-        $resolved | Where-Object {$_.Strings -like '*v=DMARC1*'}
+    function Check-DmarcRecordExists ([psobject]$DNSData) {
+        $DNSData.DMARC | Where-Object {$_.Strings -like '*v=DMARC1*'} -ErrorAction SilentlyContinue
     }
 
-    function Get-DmarcRecordText ([string]$DomainName) {
-        if ([string]::IsNullOrEmpty($DomainName)) {
-            throw "No domain name specified for DMARC record check"
-        }
-
-        $resolved = Resolve-DnsName -Name "_dmarc.$($DomainName)" -Type TXT -ErrorAction SilentlyContinue -WarningAction SilentlyContinue
-
-        $resolved | Where-Object {$_.Strings -like '*v=DMARC1*'} | Select-Object -ExpandProperty Strings
+    function Get-DmarcRecordText ([psobject]$DNSData) {
+        $DNSData.DMARC | Where-Object {$_.Strings -like '*v=DMARC1*'} -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Strings -ErrorAction SilentlyContinue
     }
 
-    function Determine-ExchangeOnline ([string]$DomainName) {
-       $isOffice365Tenant = "No"
+    function Determine-DmarcPolicy ([psobject]$dnsData) {
+        $record = $DNSData.DMARC | Where-Object {$_.Strings -like '*v=DMARC1*'} -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Strings -ErrorAction SilentlyContinue
+
+        if ($record -eq $null) { return "N/A" }
+
+        $domainPolicy = $record.Split(';') | Where-Object {$_ -like "* p=*"}
+
+        if ($domainPolicy) {
+            $domainPolicy = $domainPolicy.Replace(' ','')
+            $domainPolicy = $domainPolicy.Replace('p=','')
+            $domainPolicy.ToUpper()
+        }
+    }
+
+    function Determine-DmarcSubdomainPolicy ([psobject]$dnsData) {
+        $record = $DNSData.DMARC | Where-Object {$_.Strings -like '*v=DMARC1*'} -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Strings -ErrorAction SilentlyContinue
+
+        if ($record -eq $null) { return "N/A" }
+
+        $subDomainPolicy = $record.Split(';') | Where-Object {$_ -like "*sp=*"}
+
+        if ($subDomainPolicy) {
+            $subDomainPolicy = $subDomainPolicy.Replace(' ','')
+            $subDomainPolicy = $subDomainPolicy.Replace('sp=','')
+            $subDomainPolicy.ToUpper()
+        }
+    }
+
+    function Determine-ExchangeOnline ([psobject]$DNSData) {
+        $isOffice365Tenant = "No"
     
-        $msoidRecord = Resolve-DnsName "msoid.$($DomainName)" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue | Where-Object {$_.NameHost -like '*clientconfig.microsoftonline*'}
+        $msoidRecord = $DNSData.MSOID | Where-Object {$_.NameHost -like '*clientconfig.microsoftonline*'} -ErrorAction SilentlyContinue
         if ($msoidRecord) {$isOffice365Tenant = 'Possibly'}
 
-        $txtVerificationRecord = Resolve-DnsName $DomainName -Type TXT -ErrorAction SilentlyContinue -WarningAction SilentlyContinue | Where-Object {$_.Strings -like 'MS=ms*'}
+        $txtVerificationRecord = $DNSData.TXT | Where-Object {$_.Strings -like 'MS=ms*'} -ErrorAction SilentlyContinue
         if ($txtVerificationRecord) {$isOffice365Tenant = 'Possibly'}
 
-        $mdmRecord = Resolve-DnsName -Name "enterpriseregistration.$DomainName" -Type CNAME -ErrorAction SilentlyContinue -WarningAction SilentlyContinue | Where-Object {$_.NameHost -eq 'enterpriseregistration.windows.net '}
+        $mdmRecord = $DNSData.ENTERPRISEREGISTRATION | Where-Object {$_.NameHost -eq 'enterpriseregistration.windows.net '} -ErrorAction SilentlyContinue
         if ($mdmRecord) {$isOffice365Tenant = 'Likely'}
 
-        $autoDiscoverRecord = Resolve-DnsName -Name "autodiscover.$DomainName" -Type CNAME -ErrorAction SilentlyContinue -WarningAction SilentlyContinue | Where-Object {$_.NameHost -eq 'autodiscover.outlook.com'}
+        $autoDiscoverRecord = $DNSData.AUTODISCOVER | Where-Object {$_.NameHost -eq 'autodiscover.outlook.com'} -ErrorAction SilentlyContinue
         if ($autoDiscoverRecord) {$isOffice365Tenant = 'Yes'}
 
-        $mxRecords = Check-MxRecord $DomainName | Where-Object {$_.Name -like '*mail.protection.outlook.com*'}
+        $mxRecords = $DNSData.MX | Where-Object {$_.Name -like '*mail.protection.outlook.com*'} -ErrorAction SilentlyContinue
         if ($mxRecords) {$isOffice365Tenant = 'Yes'}
 
-        $spfRecords = Check-SpfRecord $DomainName | Where-Object {$_.Strings -like '*spf.protection.outlook.com*'}
+        $spfRecords = Check-SpfRecordExists $DNSData | Where-Object {$_.Strings -like '*spf.protection.outlook.com*'}
         if ($spfRecords) {$isOffice365Tenant = 'Yes'}
 
         $isOffice365Tenant
     }
 
-    function Determine-MXHandler ([string]$DomainName) {
-        $mx = Resolve-DnsName $DomainName -Type MX -ErrorAction SilentlyContinue -WarningAction SilentlyContinue 
-        $lowestPreferenceMX = $mx | Sort-Object -Property Preference | Select-Object -First 1 -ExpandProperty NameExchange -ErrorAction SilentlyContinue -WarningAction SilentlyContinue
+    function Determine-MXHandler ([psobject]$DNSData) {
+        if ($DNSData.MX -eq $null) { return }
+
+        $lowestPreferenceMX = $DNSData.MX | Sort-Object -Property Preference | Select-Object -First 1 -ExpandProperty NameExchange -ErrorAction SilentlyContinue -WarningAction SilentlyContinue
 
         switch -Wildcard ($lowestPreferenceMX) {
             'aspmx*google.com' { $determination = "Google" }
@@ -173,74 +199,106 @@ begin {
             '*trendmicro*' { $determination = "Trend Micro" }
             '*pphosted*' { $determination = "Proofpoint" }
             '*securence*' { $determination = "Securence" }
-            "*$DomainName" { $determination = "Self-Hosted"}
+            "*$($DNSData.SOA.Name)" { $determination = "Self-Hosted"}
             "" { $determination = "NO MX RECORD FOUND"}
 
             $null { $determination = "NO MX RECORD FOUND"}
             Default { $determination = "Other/Undetermined" }
         }
 
-        $determination
+        return $determination
     }
 
-    function Check-DnsNameAdministrator ([string]$DomainName) {
-        Resolve-DnsName -Type SOA -Name $DomainName | Select-Object -ExpandProperty NameAdministrator
+    function Check-DnsNameAdministrator ([psobject]$DNSData) {
+        $DNSData.SOA | Select-Object -First 1 -ExpandProperty NameAdministrator -ErrorAction SilentlyContinue
     }
 
-    function Check-DnsHostingProvider ([string]$DomainName) {
-        (Resolve-DnsName $DomainName -Type NS | Where-Object {$_.NameHost -ne $null} | Select-Object -ExpandProperty NameHost) -join ','
-    }
+    function Check-DnsHostingProvider ([psobject]$DNSData) {
+        if ($DNSData.NS) {
+            $nameServerRecords = ($DNSData.NS | Where-Object {$_.NameHost -ne $null} -ErrorAction SilentlyContinue | Select-Object -ExpandProperty NameHost -ErrorAction SilentlyContinue)
 
-    function Determine-O365DomainGuid ([string]$DomainName) {
-        $isOffice365Tenant = Determine-ExchangeOnline -DomainName $DomainName
-   
-        if ($isOffice365Tenant -eq 'No') {
-            "N/A"
-        } else {
-            $nameExchange = Resolve-DnsName -Name $DomainName -Type MX -ErrorAction SilentlyContinue -WarningAction SilentlyContinue | Where-Object {$_.NameExchange -ne $null} | Select-Object -ExpandProperty NameExchange
-            $isMxHandlingWithEOP = $nameExchange.Contains('mail.protection.outlook.com')
-
-            if ($isMxHandlingWithEOP -eq $false) {
-                "Undetermined"
-            } else {
-                $nameExchange.Replace('.mail.protection.outlook.com','')
-            }
+            if ($nameServerRecords) { $nameServerRecords -join ',' }
         }
     }
 
-    function Determine-O365Dkim ([string]$DomainName) {
-        $isOffice365Tenant = Determine-ExchangeOnline -DomainName $DomainName
+    function Determine-O365DomainGuid ([psobject]$DNSData) {
+        $isOffice365Tenant = Determine-ExchangeOnline -DNSData $DNSData
    
-        if ($isOffice365Tenant -eq 'No') {
-            "N/A"
-        } else {
-            $selector1,$selector2 = 1..2 | ForEach-Object {Resolve-DnsName "selector$_._domainkey.$DomainName" -ErrorAction SilentlyContinue | Where-Object {$_.NameHost -ne $null} | Select-Object -ExpandProperty NameHost}
+        if ($isOffice365Tenant -eq 'No') { return "N/A" }
 
-            if (($selector1 -ne $null) -and ($selector2 -ne $null)) {
-                $True
-            } else {
-                "Undetermined"
-            }
+        $lowestPreferenceMX = $DNSData.MX | Where-Object {$_.NameExchange -ne $null} -ErrorAction SilentlyContinue | Sort-Object -Property Preference | Select-Object -First 1 -ErrorAction SilentlyContinue
+        $nameExchange = $lowestPreferenceMX | Select-Object -ExpandProperty NameExchange -ErrorAction SilentlyContinue
+            
+        if ($nameExchange -eq $null) { return "Undetermined" }
+
+        if ($nameExchange.Contains('mail.protection.outlook.com')) {
+            $record = $nameExchange | Where-Object {$_ -like '*.mail.protection.outlook.com'} | Select -First 1
+            if ($record) { $record.Replace('.mail.protection.outlook.com','') }
+        } else { return "Undetermined" }
+    }
+
+    function Determine-O365Dkim ([psobject]$DNSData) {
+        $isOffice365Tenant = Determine-ExchangeOnline -DNSData $DNSData
+   
+        if ($isOffice365Tenant -eq 'No') { return "N/A" } 
+
+        if (($DNSData.O365DKIM.SELECTOR1 -ne $null) -and ($DNSData.O365DKIM.SELECTOR2 -ne $null)) {
+            $True
+        } else {
+            $false
         }
+
+    }
+
+    function Check-MtaStsRecordExists ([psobject]$dnsData) {
+        $mtaRecord = $dnsData.MTASTS | Where-Object {$_.Strings -like "v=STSv1*"} -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Strings -ErrorAction SilentlyContinue
+
+        ($mtaRecord -ne $null)
     }
 
 }
 
 process {
     foreach ($domain in $EmailDomain) {
+
+        # Collect DNS Records
+        $ErrorActionPreference = 'SilentlyContinue'
+
+        $dnsData = [psobject]@{
+            DMARC = Resolve-DnsName -Name "_dmarc.$($domain)" -Type TXT
+            MX = Resolve-DnsName -Name $domain -Type MX
+            MTASTS = Resolve-DnsName -Name "_mta-sts.$domain" -Type TXT
+            MSOID = Resolve-DnsName "msoid.$($domain)"
+            TXT = Resolve-DnsName $domain -Type TXT
+            ENTERPRISEREGISTRATION = Resolve-DnsName -Name "enterpriseregistration.$domain" -Type CNAME
+            AUTODISCOVER = Resolve-DnsName -Name "autodiscover.$domain" -Type CNAME
+            SOA = Resolve-DnsName -Type SOA -Name $domain
+            NS = Resolve-DnsName $domain -Type NS
+            O365DKIM = [psobject]@{
+                SELECTOR1 = Resolve-DnsName "selector1._domainkey.$domain" -Type CNAME
+                SELECTOR2 = Resolve-DnsName "selector2._domainkey.$domain" -Type CNAME
+            }
+        }
+
+        $ErrorActionPreference = 'Continue'
+        # Finish collecting DNS records
         
         New-Object psobject -Property ([ordered]@{
                                         'Domain' = $domain;
-                                        'MX Handler' = (Determine-MXHandler $domain);
-                                        'SPF Record Exists?'= ((Check-SpfRecord -DomainName $domain) -ne $null);
-                                        'SPF Record'= (Get-SpfRecordText -DomainName $domain);
-                                        'DMARC Record Exists?'= ((Check-DmarcRecord -DomainName $domain) -ne $null);
-                                        'DMARC Record' = (Get-DmarcRecordText -DomainName $domain);
-                                        'DNS Registrar' = (Check-DnsNameAdministrator -DomainName $domain);
-                                        'DNS Host' = (Check-DnsHostingProvider -DomainName $domain);
-                                        'Exchange Online?'= (Determine-ExchangeOnline -DomainName $domain);
-                                        'O365 Tenant Guid' = (Determine-O365DomainGuid -DomainName $domain);
-                                        'O365 DKIM Enabled?' = (Determine-O365Dkim -DomainName $domain);
+                                        'MX Handler' = (Determine-MXHandler $dnsData);
+                                        'SPF Record Exists?' = ((Check-SpfRecordExists $dnsData) -ne $null);
+                                        'SPF Record' = (Get-SpfRecordText $dnsData);
+                                        'SPF Mechanism (Mode)' = (Determine-SpfRecordMode $dnsData);
+                                        'DMARC Record Exists?'= ((Check-DmarcRecordExists $dnsData) -ne $null);
+                                        'DMARC Record' = (Get-DmarcRecordText $dnsData);
+                                        'DMARC Domain Policy' = (Determine-DmarcPolicy $dnsData);
+                                        'DMARC Subdomain Policy' = (Determine-DmarcSubdomainPolicy $dnsData);
+                                        'DNS Registrar' = (Check-DnsNameAdministrator $dnsData);
+                                        'DNS Host' = (Check-DnsHostingProvider $dnsData);
+                                        'Exchange Online?'= (Determine-ExchangeOnline $dnsData);
+                                        'O365 Tenant Guid' = (Determine-O365DomainGuid $dnsData);
+                                        'O365 DKIM Enabled?' = (Determine-O365Dkim $dnsData);
+                                        'MTA-STS Record Exists?' = (Check-MtaStsRecordExists $dnsData)
                                         })
     }
 }
